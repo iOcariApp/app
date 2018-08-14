69808633329169cb752bd9ebf203c96da0b4ed4c
Enzo Ferey <hello@enzoferey.com>
2018-08-14 15:34:53 +0200

    Removed unused var.

--- src/pages/Register/Page1.js
+++ src/pages/Register/Page1.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View } from "react-native";
 import styles from "./Register.style";
 
 import TextInput from "components/TextInput";
